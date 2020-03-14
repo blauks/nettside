8b875665dbd8c41404e368950643aacb129a961e
Jonathan Brooks <brooks.jonathan1@gmail.com>
2020-03-14 22:56:59 +0100

    testing

--- meg/src/App.js
+++ meg/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Jonathan Brooks</p>
+      <p>Informatics student, NTNU Trondheim</p>
+      <div className="sosialeMedier">
+          <p>Linkedin</p>
+          <p>Instagram</p>
+          <p>Github</p>
+      </div>
     </div>
   );
 }
