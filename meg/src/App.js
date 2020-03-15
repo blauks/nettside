ef0a6c44713c82a1eaf6802876d75a6f8a50c48b
Jonathan Brooks <brooks.jonathan1@gmail.com>
2020-03-15 16:00:24 +0100

    La til lenker

--- meg/src/App.js
+++ meg/src/App.js
@@ -4,12 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Jonathan Brooks</p>
+      <h1>Jonathan Brooks</h1>
       <p>Informatics student, NTNU Trondheim</p>
       <div className="sosialeMedier">
-          <p>Linkedin</p>
-          <p>Instagram</p>
-          <p>Github</p>
+        <a href="https://www.linkedin.com/in/brooks-jonathan/" target="_blank" rel="noopener noreferrer">
+          <img className="sosiallogo" alt="Linkedin" src={process.env.PUBLIC_URL + "/LinkedinLogo.png"}/>
+        </a>
+        <a href="https://www.instagram.com/jonybrooks/" target="_blank" rel="noopener noreferrer">
+          <img className="sosiallogo" alt="Instagram" src={process.env.PUBLIC_URL + "/InstagramLogo.png"}/>
+        </a>
+        <a href="https://github.com/blauks" target="_blank" rel="noopener noreferrer">
+          <img className="sosiallogo" alt="Github" src={process.env.PUBLIC_URL + "/GithubLogo.png"}/>
+        </a>
       </div>
     </div>
   );
