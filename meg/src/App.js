6382bc6f9d5b73549307870136fff43e41396371
Jonathan Brooks <brooks.jonathan1@gmail.com>
2020-03-15 23:40:00 +0100

    added circles

--- meg/src/App.js
+++ meg/src/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [xds, addxds] = useState({
+    obj: [],
+  })
+
+  const handleMouseClick = event => {
+    addxds({
+      obj: [...xds.obj ,{
+        left: event.clientX,
+        top: event.clientY,
+        position: 'absolute'
+      }]
+    })
+  }
+  
   return (
-    <div className="App">
-      <h1>Jonathan Brooks</h1>
-      <p>Informatics student, NTNU Trondheim</p>
-      <div className="sosialeMedier">
-        <a href="https://www.linkedin.com/in/brooks-jonathan/" target="_blank" rel="noopener noreferrer">
-          <img className="sosiallogo" alt="Linkedin" src={process.env.PUBLIC_URL + "/LinkedinLogo.png"}/>
-        </a>
-        <a href="https://www.instagram.com/jonybrooks/" target="_blank" rel="noopener noreferrer">
-          <img className="sosiallogo" alt="Instagram" src={process.env.PUBLIC_URL + "/InstagramLogo.png"}/>
-        </a>
-        <a href="https://github.com/blauks" target="_blank" rel="noopener noreferrer">
-          <img className="sosiallogo" alt="Github" src={process.env.PUBLIC_URL + "/GithubLogo.png"}/>
-        </a>
+    <div className="App" id="App" onClick={handleMouseClick}>
+      <div className="circles">
+        {xds.obj.map(function(d, i){
+          return <div className="circle" key={i} style={d}/>
+        })}
+      </div>
+      <div className="content">
+        <h1>Jonathan Brooks</h1>
+        <p>Informatics student, NTNU Trondheim</p>
+        <div className="sosialeMedier">
+          <a href="https://www.linkedin.com/in/brooks-jonathan/" target="_blank" rel="noopener noreferrer">
+            <img className="sosiallogo" alt="Linkedin" src={process.env.PUBLIC_URL + "/LinkedinLogo.png"}/>
+          </a>
+          <a href="https://www.instagram.com/jonybrooks/" target="_blank" rel="noopener noreferrer">
+            <img className="sosiallogo" alt="Instagram" src={process.env.PUBLIC_URL + "/InstagramLogo.png"}/>
+          </a>
+          <a href="https://github.com/blauks" target="_blank" rel="noopener noreferrer">
+            <img className="sosiallogo" alt="Github" src={process.env.PUBLIC_URL + "/GithubLogo.png"}/>
+          </a>
+      </div>
       </div>
     </div>
   );
