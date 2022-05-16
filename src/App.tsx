9dd4aa0e4992bcf3951bbc555b569e1716ef29d0
Jonathan Brooks <brooks.jonathan1@gmail.com>
2022-05-16 22:53:15 +0200

    Add just norwegian CV :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import Title, { FontSize } from "./components/atoms/Title";
-import Content from "./components/organisms/Content";
-import data from "./MyFirstLocalDatabase/data.json";
+import "./App.css";
+import CVSvg from "./CV_NORSK.svg";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen p-4 App bg-gradient-to-t from-green-50 to-blue-50">
-      <Title content="Jonathan Brooks" size={FontSize.LARGE} />
-      <div className="grid self-center gap-10 mt-6 auto-cols-auto md:w-4/5">
-        <img src="./meg.jpg" alt="Me" className="w-1/4 justify-self-center" />
-        <Content title="WHO_AM_I?" text={data.info} />
-        <Content title="PROGRAMMING_LANGUAGES" list={data.programming} />
-        <Content title="TECHNOLOGIES" list={data.technologies} />
-        <Content title="WORK_EXPERIENCE" work={data.work} />
-        <Content title="CONTACT_ME" />
-      </div>
+    <div className="App">
+      <img src={CVSvg} alt="CV" className="cv" />
     </div>
   );
 }
